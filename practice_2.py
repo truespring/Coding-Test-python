95aeee07e47ffefba4ea171588dd9d9d5bee4abf
truespring <true_spring_@naver.com>
2020-11-12 23:35:08 +0900

    python 기초 다지기

---
+++ practice_2.py
@@ -0,0 +1,11 @@
+print(2**3) # 2^3 = 8
+print(5//3) # 몫 구하기 = 1
+
+print((3 > 0) and (3 < 5)) # True
+print((3 > 0) & (3 < 5)) # True
+
+print((3 > 0) or (3 > 5)) # True
+print((3 > 0) | (3 > 5)) # True
+
+print(5 > 4 > 3) # True
+print(5 > 4 > 7) # False
