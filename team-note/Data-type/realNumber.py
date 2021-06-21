dadcc9b62ec89270300485db8ccd9b6411622622
truespring <true_spring_@naver.com>
2021-06-21 19:56:21 +0900

    실수 추가 내용, 수 자료형의 연산 추가

--- team-note/Data-type/realNumber.py
+++ team-note/Data-type/realNumber.py
@@ -13,3 +13,19 @@ print(a)  # 5.0
 # 정수부가 0일 떼 0을 생략
 a = -.7
 print(a)  # -0.7
+
+a = 0.3 + 0.6
+print(a)  # 0.8999999999999999
+
+if a == 0.9:
+    print(True)
+else:
+    print(False)  # False
+
+a = 0.3 + 0.6
+print(round(a, 4))  # 0.9
+
+if round(a, 4) == 0.9:
+    print(True)  # True
+else:
+    print(False)
